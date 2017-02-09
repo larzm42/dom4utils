0bde18d9f9542c4cdf2e9e3ce95af6e17565471b
unknown <larzsp@gmail.com>
2017-02-09 15:57:35 -0500

    Update to 4.29.

--- src/Starts.java
+++ src/Starts.java
@@ -15,30 +15,30 @@
  */
 
 public class Starts {
-	public static final long ITEM = 0x0c69ba8l;
+	public static final long ITEM = 0x0c6b1b8l;
 	public static final int ITEM_COUNT = 391;
 	
-	public static final long MONSTER = 0x0cce488l;
-	public static final long MONSTER_MAGIC = 0x0fe0150l;
-	public static final long ITEM_AND_MONSTER_DESC = 0x01a3848l;
-	public static final long ITEM_AND_MONSTER_DESC_INDEX = 0x02b7388l;
+	public static final long MONSTER = 0x0ccfd70l;
+	public static final long MONSTER_MAGIC = 0x0fe1aa8l;
+	public static final long ITEM_AND_MONSTER_DESC = 0x01a4c28l;
+	public static final long ITEM_AND_MONSTER_DESC_INDEX = 0x02b8778l;
 	public static final int MONSTER_COUNT = 2936;
 	
 	public static final long MONSTER_TRS_INDEX = 0x001707cl;
 	
-	public static final long SITE = 0x0f06e08l;
+	public static final long SITE = 0x0f08748l;
 	public static final int SITE_COUNT = 1052;
 	
-	public static final long NAMES = 0x0b00e84l;
-	public static final long FIXED_NAMES = 0x0c4601cl;
+	public static final long NAMES = 0x0b023fcl;
+	public static final long FIXED_NAMES = 0x0c47594l;
 	
 	public static final int NAMES_COUNT = 161;
 	
-	public static final long SPELL_DESC = 0x02c2f08l;
-	public static final long SPELL_DESC_INDEX = 0x0301630l;
+	public static final long SPELL_DESC = 0x02c42f8l;
+	public static final long SPELL_DESC_INDEX = 0x0302a28l;
 
-	public static final long EVENT = 0x0307420l;
+	public static final long EVENT = 0x0308818l;
 	
-	public static final long MERCENARY = 0x0cb2baal;
+	public static final long MERCENARY = 0x0cb43f2l;
 	public static final int MERCENARY_COUNT = 68;
 }
